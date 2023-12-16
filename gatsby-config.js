c57b775127471be4119470af40e5e078f6cda15f
kiki3700 <5575492q@gmail.com>
2023-12-16 21:48:13 +0000

    chapter 4 clear

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,5 @@ module.exports = {
     title: `My Frist Gatsby Site`,
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
-}
+  plugins: ["gatsby-plugin-image", "gatsby-plugin-sharp"],
+};
